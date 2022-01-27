a5b4bc8dccfae9b7aecfc68622c341b3ff191182
LorenzoBuongiorno <lorenzo.buongiorno00@gmail.com>
2022-01-27 17:27:44 +0100

    footer link section

--- src/components/FooterBox.vue
+++ src/components/FooterBox.vue
@@ -44,6 +44,58 @@
                 </li>
             </ul>
         </div>
+        <div class="footer-links">
+            <div class="row">
+                <div>
+                    <h1>dc comix</h1>
+                    <ul>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">comix</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h1>dc</h1>
+                    <ul>
+                        <li><a href="#">Term Of Use</a></li>
+                        <li><a href="#">Privacy policy (New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Rating</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h1>sites</h1>
+                    <ul>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h1>shop</h1>
+                    <ul>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="dc-logo-bg">
+                <img src="@/assets/img/dc-logo-bg.png" alt="">
+            </div>
+        </div>
     </footer>
 </template>
 
