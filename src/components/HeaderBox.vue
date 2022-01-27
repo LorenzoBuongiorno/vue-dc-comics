16451f1d1e9b801ff0eba7562c18854b5c78fbb6
LorenzoBuongiorno <lorenzo.buongiorno00@gmail.com>
2022-01-27 16:18:10 +0100

    hover with logo and links

--- src/components/HeaderBox.vue
+++ src/components/HeaderBox.vue
@@ -1,5 +1,23 @@
 <template>
-    
+    <header>
+        <div class="header-container">
+            <div class="logo">
+                <img src="@/assets/img/dc-logo.png" alt="">
+            </div>
+            <ul>
+                <li><a href="#">characters</a></li>
+                <li><a href="#">comics</a></li>
+                <li><a href="#">movies</a></li>
+                <li><a href="#">tv</a></li>
+                <li><a href="#">games</a></li>
+                <li><a href="#">collectibles</a></li>
+                <li><a href="#">videos</a></li>
+                <li><a href="#">fans</a></li>
+                <li><a href="#">news</a></li>
+                <li><a href="#">shop</a></li>
+            </ul>
+        </div>
+    </header>
 </template>
 
 <script>
@@ -8,6 +26,7 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import '@/style/variables.scss';
+@import '@/style/header.scss';
 </style>
