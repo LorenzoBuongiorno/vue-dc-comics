c2cda2f45521594caa5159411f5d9d68f60848f6
LorenzoBuongiorno <lorenzo.buongiorno00@gmail.com>
2022-01-28 13:02:05 +0100

    main and current series label

---
+++ src/components/MainBox.vue
@@ -0,0 +1,33 @@
+<template>
+    <main>
+        <div class="label">current series</div>
+    </main>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+@import '@/style/variables.scss';
+
+main{
+    position: relative;
+    height: 100px;
+    .label{
+        position: absolute;
+        top: -20px;
+        left: 200px;
+        padding: 10px 30px;
+        color: $text-light-color;
+        font-size: 30px;
+        font-weight: bold;
+        background-color: $main-color;
+        text-transform: uppercase;
+
+    }
+
+}
+</style>
